3c4f106e794a854869a7c5ceb036c9883bac32ef
HassanElwany <elwany_hassan@hotmail.com>
2023-12-18 09:43:36 +0300

    wrap up the project

--- main.py
+++ main.py
@@ -1,18 +1,46 @@
 import smtplib
 from dotenv import load_dotenv
 import os
+import datetime as dt
+import random
+import pandas as pd
+import os
 
 load_dotenv()
 
 
-
-
-my_email = "hassan.elwany1987@gmail.com"
+now = dt.datetime.now()
+data = pd.read_csv("./birthdays.csv")
+my_email = "hassanwebdevt@gmail.com"
 my_password = os.getenv("PASSWORD")
+data_list = data.to_dict('records')
 
-connection = smtplib.SMTP("smtp.gmail.com")
 
-connection.starttls()
-connection.login(user=my_email, password=my_password)
-connection.sendmail(from_addr=my_email, to_addrs="elwany_hassan@hotmail.com", msg="Hello")
-connection.close()
+for row in data_list:
+    email = row['email']
+    year = int(row['year'])
+    month = int(row['month'])
+    day = int(row['day'])
+    name = row['name']
+
+    if now.month == month and now.day == day:
+        random_message = random.choice(os.listdir("./letter_templates"))
+        with open(f"./letter_templates/{random_message}", "r") as file:
+            message = file.readlines()
+            modified_line = message[0].replace('[NAME]', name)
+            modified_message = modified_line + ''.join(message[1:])
+
+        with open("quotes.txt", "r") as file:
+            quotes_lines = file.readlines()
+            quote = random.choice(quotes_lines).split("-")
+
+            with smtplib.SMTP("smtp.gmail.com") as connection:
+
+                connection.starttls()
+                connection.login(user=my_email, password=my_password)
+                connection.sendmail(
+                    from_addr=my_email,
+                    to_addrs=f"{email}",
+                    msg=f"Subject:BirthDay!\n\n {modified_message}"
+
+                )
